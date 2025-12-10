e2edb1f3e248a3776f54de7eaba411f2b1f04b15
ASUKA　奈良 <asukanara2025@gmail.com>
2025-12-10 12:23:12 +0900

    Add files via upload

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/svg-animator/sw.js', { scope: '/svg-animator/' })})}
